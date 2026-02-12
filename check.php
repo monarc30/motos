8caf61b310e456afe488c075e4b1ef092e3e5564
Fernando <segenfer@gmail.com>
2026-02-12 11:07:48 -0300

    Ajustes diversos

--- check.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-$logDir = __DIR__ . '/storage/logs';
-if (!is_dir($logDir)) {
-    mkdir($logDir, 0755, true);
-}
-file_put_contents($logDir . '/php_errors.log', date('Y-m-d H:i:s') . " check.php executado\n", FILE_APPEND);
-echo "OK - PHP rodando em " . __DIR__;
